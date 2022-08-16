6ff03385ee0799a3f9a8c30717c9a5b016af7692
Kaushan Ranasinghe <kaushan.gr@gmail.com>
2022-08-16 12:56:59 +0530

    Create bananas.py

---
+++ docs/bananas.py
@@ -0,0 +1,8 @@
+def peel():
+  return 'Here\'s a delicious banana!'
+
+def dip_in_chocolate():
+  return 'Here\'s a delicious banana, dipped in chocolate!'
+
+def sell():
+  return 'There\'s always money in the banana stand'
