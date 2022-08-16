cc9ced4324cb38f894a9f0757421ccc611d2e2cb
Kaushan Ranasinghe <kaushan.gr@gmail.com>
2022-08-16 18:42:49 +0530

    Update say_sup.py

--- docs/say_sup.py
+++ docs/say_sup.py
@@ -2,4 +2,4 @@ def say_sup():
   print(f'My __name__ is {__name__}')
 
 if __name__ == '__main__': # executes only if called function is main function, otherwise stays dormant on import
-say_sup()
+    say_sup()
