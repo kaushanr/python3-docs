880dc1004037371a1bc4084990aa8580248eec3b
Kaushan Ranasinghe <kaushan.gr@gmail.com>
2022-08-16 17:38:01 +0530

    Update ugly_code.py

--- docs/ugly_code.py
+++ docs/ugly_code.py
@@ -14,10 +14,10 @@ def example2(): return ('' in {'f': 2}) in {'has_key() is deprecated': True};
 class Example3(   object ):
     def __init__   (self, bar):
     #Comments should have a space after the hash.
-    if bar:  bar += 1;  bar = bar * bar   ;return bar
-    else:
-            some_string = """
-                       Indentation in multiline strings should not be touched.
-Only actual code should be reindented.
-"""
-            return (sys.path, some_string)
+        if bar:  bar += 1;  bar = bar * bar   ;return bar
+        else:
+                some_string = """
+                           Indentation in multiline strings should not be touched.
+    Only actual code should be reindented.
+    """
+                return (sys.path, some_string)
