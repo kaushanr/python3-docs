05297121c010edd3be7e64e38400097eebb3e57d
Kaushan Ranasinghe <kaushan.gr@gmail.com>
2022-08-17 10:01:34 +0530

    Update heading_art.py

--- docs/heading_art.py
+++ docs/heading_art.py
@@ -11,19 +11,21 @@ def install_termcolor():
     '''
 
 def heading(text,my_color=False):
-  if __name__ == '__main__':
-    import pyfiglet
-    import termcolor
-    from random import choice
-    
-    if type(my_color) == int:
-      raise TypeError('Please enter a valid color!')
-    elif not my_color:
-      color = choice(('red','green','yellow','blue','magenta','cyan','white')) 
-    elif my_color not in ('red','green','yellow','blue','magenta','cyan','white'):
-      raise ValueError('Color not in valid range')
-    else:
-      color = my_color
-    rendered = pyfiglet.figlet_format(text, font='standard')
-    colored_text = termcolor.colored(rendered,color)
-    return colored_text
+  import pyfiglet
+  import termcolor
+  from random import choice
+
+  if type(my_color) == int:
+    raise TypeError('Please enter a valid color!')
+  elif not my_color:
+    color = choice(('red','green','yellow','blue','magenta','cyan','white')) 
+  elif my_color not in ('red','green','yellow','blue','magenta','cyan','white'):
+    raise ValueError('Color not in valid range')
+  else:
+    color = my_color
+  rendered = pyfiglet.figlet_format(text, font='standard')
+  colored_text = termcolor.colored(rendered,color)
+  print(colored_text)
+
+if __name__ == '__main__':
+  heading()
