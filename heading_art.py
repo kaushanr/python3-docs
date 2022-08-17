30674a6cd213f9b1894f78cb4055f82fb12f6f4b
Kaushan Ranasinghe <kaushan.gr@gmail.com>
2022-08-17 08:43:36 +0530

    Create heading_art.py

---
+++ heading_art.py
@@ -0,0 +1,21 @@
+defdef install_pyfiglet():
+  if __name__ == '__main__':
+    !pip install pyfiglet
+
+def install_termcolor():
+  if __name__ == '__main__':
+    !pip install termcolor
+
+def heading(text,my_color=None):
+  if __name__ == '__main__':
+    import pyfiglet
+    import termcolor
+    from random import choice
+    if my_color == None:
+      color = choice(('red','green','yellow','blue','magenta','cyan','white')) 
+    elif my_color not in ('red','green','yellow','blue','magenta','cyan','white'):
+    
+    color = my_color
+    rendered = pyfiglet.figlet_format(text, font='standard')
+    colored_text = termcolor.colored(rendered,color)
+    print(colored_text)
