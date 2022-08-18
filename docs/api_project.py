22395984d24490ab6ef1d5e50dbb64bb7b121047
Kaushan Ranasinghe <kaushan.gr@gmail.com>
2022-08-18 06:52:50 +0530

    Update api_project.py

--- docs/api_project.py
+++ docs/api_project.py
@@ -16,27 +16,27 @@ try: # contains error handling for pyfiglet installation
 except ModuleNotFoundError as err:
     print(err)
     print('Please install \'pyfiglet\' module to continue - >!pip install pyfiglet<')
-
-category = input('Let me tell you a joke! Give me a topic : ')
-print()
-print('Disclaimer** Severe Cringe Alert!','\n')
-
-url = 'https://icanhazdadjoke.com/search?'
-response = requests.get(url,
-                        headers = {'Accept':'application/json'},
-                        params = {'term':category}
-                        )
-
-data = response.json()
-
-if data['total_jokes']:
-
-  from random import choice
-  from termcolor import colored
-
-  print(f'I\'ve got {data["total_jokes"]} jokes about {category}. Here\'s one : ')
-  joke = choice(data['results'])['joke']
-  result = colored(joke,color = 'red',attrs = ['bold'])
-  print(result)
 else:
-  print(f'Sorry, I don\'t have any jokes about {category}!. Please try again.')
+    category = input('Let me tell you a joke! Give me a topic : ')
+    print()
+    print('Disclaimer** Severe Cringe Alert!','\n')
+
+    url = 'https://icanhazdadjoke.com/search?'
+    response = requests.get(url,
+                            headers = {'Accept':'application/json'},
+                            params = {'term':category}
+                            )
+
+    data = response.json()
+
+    if data['total_jokes']:
+
+      from random import choice
+      from termcolor import colored
+
+      print(f'I\'ve got {data["total_jokes"]} jokes about {category}. Here\'s one : ')
+      joke = choice(data['results'])['joke']
+      result = colored(joke,color = 'red',attrs = ['bold'])
+      print(result)
+    else:
+      print(f'Sorry, I don\'t have any jokes about {category}!. Please try again.')
